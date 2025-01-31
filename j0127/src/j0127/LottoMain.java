ad4d4805fcebe2e5f914ffb30baee9b827317a12
parkmk0615 <parkmk0615@naver.com>
2025-01-31 11:36:36 +0900

    0127

---
+++ j0127/src/j0127/LottoMain.java
@@ -0,0 +1,24 @@
+package j0127;
+
+public class LottoMain {
+
+	public static void main(String[] args) {
+		//1. ball 객체
+		//2. ball_deck 객체 
+		//- ball 45개 생성, ball숫자 입력, ball 섞기
+		//- 6개 로또 번호 추출, 6개 숫자 입력, 로또 번호와 입력번호 비교
+		//- 출력화면이 있음.
+		//3. main에서 로또 프로그램을 실행해서 6개숫자입력, 로또번호와 비교 몇개 맞췄는지 확인
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ j0127/src/j0127/ball.java
@@ -0,0 +1,5 @@
+package j0127;
+
+public class ball {
+	
+}
